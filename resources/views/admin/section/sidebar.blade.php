b2036eb842b5a1c6b3f1fa0c6520639a92899de9
naser <na3r.jafari@gmail.com>
2022-03-10 09:10:02 +0330

    sff

--- resources/views/admin/section/sidebar.blade.php
+++ resources/views/admin/section/sidebar.blade.php
@@ -169,7 +169,7 @@
                         <a href="{{route('repair.index')}}" class="nav-link {{(Route::currentRouteName()=='repair.index')?'active':''}} ">
                             <i class="nav-icon fa fa-cogs"></i>
                             <p>
-                                ثبت  sd
+                                ثبت  sdd
                             </p>
                         </a>
                     </li>
